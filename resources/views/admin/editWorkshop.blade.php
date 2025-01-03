7757157c9dc4f596f9a782c634fd7c9e594c56a2
disyudhis <yudhistiraaaa20@gmail.com>
2025-01-03 13:12:45 +0700

    add icon for each layout

--- resources/views/admin/editWorkshop.blade.php
+++ resources/views/admin/editWorkshop.blade.php
@@ -4,6 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="{{ asset('img/about.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Admin - Edit Pelatihan</title>
--- resources/views/wub/dashboardWub.blade.php
+++ resources/views/wub/dashboardWub.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('img/about.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
     <title>WUB - Dashboard</title>
